3ecd4f1cbf768be6bbd0fe0d98e145c0854e2e46
nikhitkumar <nikhitkumar00@gmail.com>
2023-06-06 08:04:23 +0530

    added bill id api

---
+++ billid.php
@@ -0,0 +1,36 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "invo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT MAX(bill_id) AS newbillid FROM bill_items";
+$result = $conn->query($sql);
+
+if ($result === false) {
+    echo json_encode(array(
+        "status" => "error",
+        "message" => "Error finding bill_id: " . $conn->error
+    ));
+} else {
+    $row = $result->fetch_assoc();
+    $billid = $row['newbillid'];
+    echo json_encode(array(
+        "status" => "success",
+        "billId" => $billid
+    ));
+}
+
+$conn->close();
+?>
