e8a451fa33ccdd155f3811de98c9a681f3c6b87a
nikhitkumar <nikhitkumar00@gmail.com>
2023-06-07 22:07:56 +0530

    added billitem log api

---
+++ billitemslog.php
@@ -0,0 +1,35 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "invo";
+
+$billId = $_GET['billId'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->e) {
+    die("Connection failed: " . $conn->e);
+}
+
+$sql = "SELECT name,bill_quantity,amount from bill_items join stocks on bill_items.item_id = stocks.item_id where bill_id = $billId";
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+header("Content-Type: application/json");
+
+echo json_encode($data);
+?>
