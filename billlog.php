d6342d1b5a1b7ee20f05e957fe363b6cc419a1b4
nikhitkumar <nikhitkumar00@gmail.com>
2023-06-07 21:35:13 +0530

    added new api for bill log

---
+++ billlog.php
@@ -0,0 +1,33 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "invo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->e) {
+    die("Connection failed: " . $conn->e);
+}
+
+$sql = "SELECT * FROM bills";
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+header("Content-Type: application/json");
+
+echo json_encode($data);
+?>
